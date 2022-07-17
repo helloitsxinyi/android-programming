db008221b8060dfa8af3dc75f2d3f4990a012340
Xin Yi Tay <xinzwerkz@gmail.com>
2022-07-17 13:48:41 +0800

    add workshop for notifs

---
+++ app/src/main/java/iss/workshop/workshop6notifications/MainActivity.java
@@ -0,0 +1,74 @@
+package iss.workshop.workshop6notifications;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String CHANNEL_ID = "888888";
+    private static final String CHANNEL_NAME = "Message Notification Channel";
+    private static final String CHANNEL_DESCRIPTION = "This channel is for displaying messages";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button notifyBtn = findViewById(R.id.notifyBtn);
+        notifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNotificationChannel();
+                createNotification();
+            }
+        });
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance);
+            channel.setDescription(CHANNEL_DESCRIPTION);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void createNotification() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+
+        builder
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Harry says")
+                .setContentText("Hi Dip SA class, how is your study?")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+        int notificationId = 11111;
+        NotificationManagerCompat mgr =
+                NotificationManagerCompat.from(this);
+        Notification notification = builder.build();
+        mgr.notify(notificationId, notification);
+
+    }
+
+}
